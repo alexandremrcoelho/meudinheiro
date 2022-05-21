705909669f3425915c45c6b87e08a4a1519e6dec
Alexandre <alexandremrc@hotmail.com>
2022-05-20 21:44:32 -0300

    Cadastro de usuarios com tela de login

--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -1,8 +1,33 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from .forms import UsuarioForm
 
 # Create your views here.
 
-
 def inicio(request):
     return render(request, 'base.html', {})
+
+
+def novo_usuario(request):
+    template_name = 'usuarios/novo_usuario.html'
+    context = {}
+    if request.method == 'POST':
+        form = UsuarioForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.set_password(user.password)
+            user.save()
+            messages.success(request, 'Contra criada com sucesso')
+            return redirect('usuarios:usuario_login')
+    else:
+        form = UsuarioForm()
+    context['form'] = form
+    return render(request, template_name, context)
+
+
+def usuario_login(request):
+    template_name = 'usuarios/usuario_login.html'
+    context = {}
+    return render(request, template_name, context)
